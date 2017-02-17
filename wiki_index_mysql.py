50ebacb9c5bb1f1dff00c0d9698d06155d1c6fd0
waynesola <waynesola@users.noreply.github.com>
2017-02-18 02:19:44 +0800

    Commit Test

--- wiki_index_mysql.py
+++ wiki_index_mysql.py
@@ -6,7 +6,6 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup as bs
 import re
 
-
 # part 1:
 resp = urlopen('https://en.wikipedia.org/wiki/Main_Page').read().decode('utf-8')  # 最好使用utf-8编码显示
 soup = bs(resp, 'html.parser')
@@ -14,3 +13,5 @@ listUrls = soup.findAll('a', href=re.compile('^/wiki/'))
 for url in listUrls:
     if not re.search("\.jpg|JPG$", url['href']):
         print(url.get_text(), ">>>", "https://en.wikipedia.org" + url['href'])
+
+print('Done!')
