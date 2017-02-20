3da70a5fb82e5e519f811e1ae7a97a5083195ad0
waynesola <waynesola@users.noreply.github.com>
2017-02-21 00:54:07 +0800

    Commit Test

--- wiki_index_mysql_fetch.py
+++ wiki_index_mysql_fetch.py
@@ -1,19 +1,16 @@
-'''
+"""
 获取MySQL数据
-'''
+"""
 
 import pymysql.cursors
 
-#获取数据库链接
-connection = pymysql.connect(host='localhost',
-                             user='root',
-                             password='88888888',
-                             database='wiki_url',
-                             charset='utf8mb4')
+# 获取数据库链接
+connection = pymysql.connect(host='localhost', user='root', password='88888888', database='wiki_url', charset='utf8mb4')
 
 try:
     # 获取会话指针
     with connection.cursor() as cursor:
+
         # 查询语句
         sql = "select `urlname`,`urlhref` from `urls` where `id` is not null"
 
@@ -26,7 +23,7 @@ try:
         print(result)
 
         # 查询3条数据
-        # result = cursor.fetchmany(size=10)
+        # result = cursor.fetchmany(size=3)
         # print(result)
 
 finally:
