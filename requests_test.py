3a60b407c3ff7d42014d5b7ddb743bba58f30102
waynesola <waynesola@users.noreply.github.com>
2017-02-26 13:09:14 +0800

    Commit Test

--- requests_test.py
+++ requests_test.py
@@ -1,8 +1,10 @@
 import requests
 
-cs_url = 'https://api.github.com'
+url = 'https://api.github.com'
 
 r = requests.get('http://a7777777ap5l0e.com')
 
 if r.status_code == 200:
     print(r.status_code)
+
+
